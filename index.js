44572252abfe6c350fde69a32e19a0dc7abe41a6
Your Name <you@example.com>
2019-06-24 13:42:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
+  const input = document.getElementById('input');
+  //go into html code and for a element with id input do something
+  input.addEventListener('click', function(event) {
+    alert('I was clicked!');
+  });  
 }
