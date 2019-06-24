0e49ce034f8bf2c27656b4e2e8b97e8e9c39976c
2daysweb <sahnunhm@gmail.com>
2019-06-24 18:37:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 function addingEventListener() {
+const input = document.getElementById('input')
+input.addEventListener('click', function(event) {
+  alert('I was clicked!')
+})
+}
+
+function greet(string) {
+
+	let hour = string.split(":")
+
+	let hours = parseInt(hour[0]) 
+
+	console.log(hours)
+
+
+	if(hours < 12) {
+		return console.log("Good Morning")
+	}
+
+	if (12 < hours && hours < 17) {
+
+		return console.log("Good Afternoon")
+	}
+
+	if (17 < hours && hours < 24) {
+
+		return console.log("Good Evening")
+	}
+
 }
